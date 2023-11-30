d18b8089fce16c44a3a00df5b894d4eae50a2b41
MWise707 <markezrawiseman@gmail.com>
2023-11-30 10:16:39 -0800

    Add input event listener that handles all 3 inputs

--- script.js
+++ script.js
@@ -18,14 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputContainer = $("<div></div>").attr("id", "inputCont");
   const inputMessage = $("<div></div>")
     .attr("id", "inputMessage")
-    .text("Change your location with one of the options below.");
+    .text("Change your location with one of the options below.")
+    .css("textAlign", "center");
   const zipInput = $("<input>")
     .attr("id", "userZip")
-    .attr("placeholder", "Change ZipCode Here");
+    .attr("placeholder", "ZipCode format: 98375");
   const latLongInput = $("<input>")
     .attr("id", "latLongInput")
-    .attr("placeholder", "lat/long: 47.1, -122.3");
-  const setLocationBtn = $("<button>💹 Get Your Forecast</button>");
+    .attr("placeholder", "Lat/Long format: 47.3, -122.3");
+  const usrNameInput = $("<input>")
+    .attr("id", "usrNameInput")
+    .attr("placeholder", "Your Name Here");
   const dropBtnContainer = $("<div></div>").attr("id", "dropBtnCont");
   const dropDownContent = $("<div></div>")
     .attr("id", "myDropdown")
@@ -33,7 +36,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const setTimeZoneBtn = $(`<button >Change Time Zone ⬇️</button>`).addClass(
     "dropbtn"
   );
-  // Default Data for my location
+  const inputNameMessage = $("<div></div>")
+    .attr("id", "inputNameMessage")
+    .text("Add your name below")
+    .css("textAlign", "center");
+  const earlyGreeting = "The Early 🐥 Gets the Worm";
+  const morningGreeting = "Good morning 🌅";
+  const afternoonGreeting = "Good afternoon 🕛";
+  const nightGreeting = "Good evening 🌃";
+  let userName = "there";
+  // Weather Variables
   let defaultData,
     timeZone,
     maxDailyTemps,
@@ -45,7 +57,17 @@ document.addEventListener("DOMContentLoaded", function () {
     localDisplayName,
     latitude,
     longitude,
-    currDate;
+    currDate,
+    isDay,
+    isRaining,
+    isSnowing,
+    cloudCover,
+    isCloudy,
+    isPartlyCloudy,
+    isOvercast,
+    timeNum,
+    chanceOfRain,
+    isClear;
   // Default Location Data
   latitude = 47.1;
   longitude = -122.32;
@@ -169,26 +191,87 @@ document.addEventListener("DOMContentLoaded", function () {
       minDailyTemps = defaultData.daily.temperature_2m_min;
       timeLabels = defaultData.daily.time;
       maxProbRain = defaultData.daily.precipitation_probability_max;
-      currTimeCard.text(`Last Updated: ${defaultData.current.time}`);
-      currTempCard.text(`${defaultData.current.temperature_2m}℉`);
+      chanceOfRain = maxProbRain[0];
+      console.log(`chance of Rain is ${chanceOfRain}%`);
+      isDay = defaultData.current.is_day === 1;
+      isRaining = defaultData.current.rain > 0;
+      isSnowing = defaultData.current.snowfall > 0;
+      isPartlyCloudy =
+        defaultData.current.cloud_cover > 19 &&
+        defaultData.current.cloud_cover < 51;
+      isCloudy =
+        defaultData.current.cloud_cover > 50 &&
+        defaultData.current.cloud_cover < 91;
+      isOvercast = defaultData.current.cloud_cover > 90;
+      isClear = defaultData.current.cloud_cover < 20;
+      console.log(defaultData.current.rain);
+      console.log(isRaining);
+      console.log(defaultData);
+      // separate time from date
+      let dateOnly, timeOnly;
+      let dtgArray = defaultData.current.time.split("T");
+      dateOnly = dtgArray[0];
+      timeOnly = dtgArray[1];
+      let timeNumArray = timeOnly.split(":");
+      console.log(timeNumArray);
+      timeNum = parseInt(timeNumArray.join(""));
+      currTitleCard.text(`On ${dateOnly} in ${localDisplayName}`);
+      currTimeCard.text(`Last Updated: ${timeOnly}`);
+      currTempCard.text(`🌡️ ${defaultData.current.temperature_2m}℉`);
       currHumidCard.text(
         `${defaultData.current.relative_humidity_2m}% Humidity`
       );
+      setDynamicBackground();
+      setDynamicMessage();
       createChart();
     });
   }
 
   fetchDataAndCreateChart();
 
+  // Logic for dynamic background
+  function setDynamicBackground() {
+    if (isDay) {
+      // Logic for daytime currContainer
+      if (isRaining) {
+        currContainer.css("background-image", "url(Graphics/day-clear.jpeg)");
+      } else {
+        currContainer.css("background-image", "url(Graphics/day-clear.jpeg)");
+      }
+    } else {
+      // Logic for nightime currContainer
+      // nightime Graphics/ezgif.com-video-to-gif.png
+      currContainer.css("background-image", "url(Graphics/night-rain.gif)");
+      // currContainer.css('background-size', 'cover');
+    }
+  }
+
+  // Logic for dynamic message
+  function setDynamicMessage() {
+    let dynamicGreeting;
+    if (timeNum <= 359) {
+      dynamicGreeting = earlyGreeting;
+    } else if (timeNum > 359 && timeNum <= 1159) {
+      dynamicGreeting = morningGreeting;
+    } else if (timeNum > 1159 && timeNum <= 1700) {
+      dynamicGreeting = afternoonGreeting;
+    } else {
+      dynamicGreeting = nightGreeting;
+    }
+    currMessageCard.text(
+      `Hey ${userName}! ${dynamicGreeting} Today's chance of Rain is ${chanceOfRain}%`
+    );
+  }
+
   // Add elements to page =======================
   // Body Start Includes Page Title
   bodyStart.append(pageTitle);
   // Body Middle Includes Chart & currContainer
+  currContainer.append(currMessageCard);
   currContainer.append(currTitleCard);
   currContainer.append(currTimeCard);
   currContainer.append(currTempCard);
   currContainer.append(currHumidCard);
-  currContainer.append(currMessageCard);
   bodyClose.append(currContainer);
   // Body Close: Input Container, DropDown
 
@@ -198,27 +281,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
   inputContainer.append(inputMessage);
   inputContainer.append(zipInput);
-  inputContainer.append(setLocationBtn);
+  // inputContainer.append(setLocationBtn);
   inputContainer.append(latLongInput);
+  inputContainer.append(inputNameMessage);
+  inputContainer.append(usrNameInput);
   bodyClose.append(inputContainer);
 
-  // Event listener for setLocationBtn
-  setLocationBtn.on("click", async function () {
-    defaultZip = parseFloat(zipInput.val());
-    await getLatLong(); // #1 Asyncronous
-    currTitleCard.text(`Currently in ${localDisplayName}`);
-    fetchDataAndCreateChart();
-  });
+  // EVENT Listeners ===================
 
+  // Event listener for setLocationBtn
+  // setLocationBtn.on("click", async function () {
+  //   defaultZip = parseFloat(zipInput.val());
+  //   await getLatLong(); // #1 Asyncronous
+  //   currTitleCard.text(`Currently in ${localDisplayName}`);
+  //   fetchDataAndCreateChart();
+  // });
+
+  // Input event listener
+
+  $("input").on("input", async function () {
+    if (
+      latLongInput.val() !== "" &&
+      latLongInput.val() !== latLongInput.prop("placeholder")
+    ) {
+      let latLongArray = latLongInput.val().split(",");
+      latitude = parseFloat(latLongArray[0]);
+      longitude = parseFloat(latLongArray[1]);
+    } else if (
+      zipInput.val() !== "" &&
+      zipInput.val() !== zipInput.prop("placeholder")
+    ) {
+      defaultZip = parseFloat(zipInput.val());
+      await getLatLong(); // AJAX call to get lat/long
+    }
+    if (
+      usrNameInput.val() !== "" &&
+      usrNameInput.val() !== usrNameInput.prop("placeholder")
+    ) {
+      userName = usrNameInput.val();
+    }
+    console.log(userName);
+    console.log(latitude, longitude);
+    console.log(defaultZip);
+  });
+  
+
+  /*
   $("#userZip").on("keydown", async function (e) {
     if (e.keyCode === 13) {
       defaultZip = parseFloat(zipInput.val());
       await getLatLong(); // #1 Asyncronous
-      currTitleCard.text(`Currently in ${localDisplayName}`);
       fetchDataAndCreateChart();
     }
   });
-
+*/
+  /*
   $("#latLongInput").on("keydown", async function (e) {
     if (e.keyCode === 13) {
       alert(
@@ -229,10 +346,10 @@ document.addEventListener("DOMContentLoaded", function () {
       longitude = parseFloat(latLongArray[1]);
       console.log(latitude, longitude);
       await getLocWithLatLong();
-      currTitleCard.text(`Currently in ${localDisplayName}`);
       await fetchDataAndCreateChart();
     }
   });
+  */
 
   // Dropdown button event listener
   setTimeZoneBtn.on("click", function () {
