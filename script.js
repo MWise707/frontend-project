180bac7af86b9cf664c12c25bb89cbf9800493c9
MWise707 <markezrawiseman@gmail.com>
2023-11-30 13:13:56 -0800

    Refactor code and add objects to hold today and future condtions

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   "use strict";
   // Declare variable and create elements
-  const body = $("body");
   const bodyStart = $("#bodyStart");
   const bodyMid = $("#bodyMid");
   const bodyClose = $("#bodyClose");
@@ -44,44 +43,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const morningGreeting = "Good morning 🌅";
   const afternoonGreeting = "Good afternoon 🕛";
   const nightGreeting = "Good evening 🌃";
-  let userName = "there";
   // Weather Variables
-  let defaultData,
-    timeZone,
-    maxDailyTemps,
-    minDailyTemps,
-    maxProbRain,
-    myChart,
-    timeLabels,
-    defaultZip,
-    localDisplayName,
-    latitude,
-    longitude,
-    currDate,
-    isDay,
-    isRaining,
-    isSnowing,
-    cloudCover,
-    isCloudy,
-    isPartlyCloudy,
-    isOvercast,
-    timeNum,
-    chanceOfRain,
-    isClear;
-  // Default Location Data
-  latitude = 47.1;
-  longitude = -122.32;
-  localDisplayName = "Puyallup, WA (default)";
-  defaultZip = 98375;
-  timeZone = "America%2FLos_Angeles";
-  let locationData = {
+  let myChart;
+  const today = {
+    userName: "there",
+  };
+  const future = {};
+  const locationData = {
     latitude: 47.1,
     longitude: -122.32,
     localDisplayName: "Puyallup, WA (default)",
     defaultZip: 98375,
     timeZone: "America%2FLos_Angeles",
   };
-  let timeZones = [
+  const timeZones = [
     "America/Anchorage",
     "America/Los_Angeles",
     "America/Denver",
@@ -89,7 +64,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "America/New_York",
     "America/Sao_Paulo",
   ];
-  currTitleCard.text(`Current Weather in ${localDisplayName}`);
+  currTitleCard.text(`Current Weather in ${locationData.localDisplayName}`);
 
   // Iterate over time zones and create drop down menu
   for (let i = 0; i < timeZones.length; i++) {
@@ -102,18 +77,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function getLatLong() {
     return new Promise((resolve, reject) => {
-      $.get(`https://geocode.maps.co/search?q=${defaultZip}`, (latLong) => {
-        if (latLong && latLong.length > 0) {
-          // Change variables for lat/long & display name
-          localDisplayName = latLong[0].display_name;
-          console.log(localDisplayName);
-          latitude = latLong[0].lat;
-          longitude = latLong[0].lon;
-          resolve();
-        } else {
-          reject(new Error("Invalid response format from geocoding API"));
+      $.get(
+        `https://geocode.maps.co/search?q=${locationData.defaultZip}`,
+        (latLong) => {
+          if (latLong && latLong.length > 0) {
+            // Change variables for lat/long & display name
+            locationData.localDisplayName = latLong[0].display_name;
+            locationData.latitude = latLong[0].lat;
+            locationData.longitude = latLong[0].lon;
+            resolve();
+          } else {
+            reject(new Error("Invalid response format from geocoding API"));
+          }
         }
-      }).fail((error) => {
+      ).fail((error) => {
         // Reject the promise if the AJAX request fails
         reject(new Error("Failed to retrieve geolocation data"));
       });
@@ -123,18 +100,21 @@ document.addEventListener("DOMContentLoaded", function () {
   // Used to find location name with LAT/LONG
   function getLocWithLatLong() {
     $.get(
-      `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}`,
+      `https://geocode.maps.co/reverse?lat=${locationData.latitude}&lon=${locationData.longitude}`,
       (locData) => {
         localDisplayName = locData.display_name;
+        locationData.localDisplayName = locData.display_name;
       }
     );
   }
+
+  //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,wind_speed_10m&daily=sunrise,sunset&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FLos_Angeles
 
   // Asynchonous Call for Weather API
   function getDefaultData() {
     return new Promise((resolve, reject) => {
       $.get(
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,cloud_cover&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=${locationData.timeZone}`,
+        `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,cloud_cover,wind_speed_10m&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=${locationData.timeZone}`,
         (data) => {
           // provide data ref
           resolve(data);
@@ -149,7 +129,7 @@ document.addEventListener("DOMContentLoaded", function () {
       myChart.destroy();
     }
     // Add dynamic text to title
-    pageTitle.text(`7 Day forecast for ${localDisplayName}`);
+    pageTitle.text(`7 Day forecast for ${locationData.localDisplayName}`);
 
     // Intitial Chart Function
     myChart = new Chart(ctx, {
@@ -159,20 +139,20 @@ document.addEventListener("DOMContentLoaded", function () {
           {
             type: "line",
             label: "Low Temperatures",
-            data: minDailyTemps,
+            data: future.minDailyTemps,
           },
           {
             type: "line",
             label: "High Temperatures",
-            data: maxDailyTemps,
+            data: future.maxDailyTemps,
           },
           {
             type: "bar",
             label: "% Chance of Precipitation",
-            data: maxProbRain,
+            data: future.maxProbRain,
           },
         ],
-        labels: timeLabels,
+        labels: future.timeLabels,
       },
       options: {
         scales: {
@@ -186,41 +166,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function fetchDataAndCreateChart() {
     getDefaultData().then((data) => {
-      defaultData = data;
-      maxDailyTemps = defaultData.daily.temperature_2m_max;
-      minDailyTemps = defaultData.daily.temperature_2m_min;
-      timeLabels = defaultData.daily.time;
-      maxProbRain = defaultData.daily.precipitation_probability_max;
-      chanceOfRain = maxProbRain[0];
-      console.log(`chance of Rain is ${chanceOfRain}%`);
-      isDay = defaultData.current.is_day === 1;
-      isRaining = defaultData.current.rain > 0;
-      isSnowing = defaultData.current.snowfall > 0;
-      isPartlyCloudy =
+      let defaultData = data;
+      // Get Forecast Data
+      future.maxDailyTemps = defaultData.daily.temperature_2m_max;
+      future.minDailyTemps = defaultData.daily.temperature_2m_min;
+      future.timeLabels = defaultData.daily.time;
+      future.maxProbRain = defaultData.daily.precipitation_probability_max;
+      // Get Current Conditions
+      today.chanceOfRain = future.maxProbRain[0];
+      today.temp = defaultData.current.temperature_2m;
+      today.isDay = defaultData.current.is_day === 1;
+      today.isRaining = defaultData.current.rain > 0;
+      today.isSnowing = defaultData.current.snowfall > 0;
+      today.isPartlyCloudy =
         defaultData.current.cloud_cover > 19 &&
         defaultData.current.cloud_cover < 51;
-      isCloudy =
+      today.isCloudy =
         defaultData.current.cloud_cover > 50 &&
         defaultData.current.cloud_cover < 91;
-      isOvercast = defaultData.current.cloud_cover > 90;
-      isClear = defaultData.current.cloud_cover < 20;
-      console.log(defaultData.current.rain);
-      console.log(isRaining);
-      console.log(defaultData);
-      // separate time from date
-      let dateOnly, timeOnly;
-      let dtgArray = defaultData.current.time.split("T");
-      dateOnly = dtgArray[0];
-      timeOnly = dtgArray[1];
-      let timeNumArray = timeOnly.split(":");
-      console.log(timeNumArray);
-      timeNum = parseInt(timeNumArray.join(""));
-      currTitleCard.text(`On ${dateOnly} in ${localDisplayName}`);
-      currTimeCard.text(`Last Updated: ${timeOnly}`);
-      currTempCard.text(`🌡️ ${defaultData.current.temperature_2m}℉`);
-      currHumidCard.text(
-        `${defaultData.current.relative_humidity_2m}% Humidity`
+      today.isOvercast = defaultData.current.cloud_cover > 90;
+      today.isClear = defaultData.current.cloud_cover < 20;
+      today.humidity = defaultData.current.relative_humidity_2m;
+
+      console.log(defaultData); // Full Weather Object
+
+      separateDTG(defaultData.current.time); // separate time from date
+
+      // Add dynamic text to Current Card
+      currTitleCard.text(
+        `On ${today.dateOnly} in ${locationData.localDisplayName}`
       );
+      currTimeCard.text(`Last Updated: ${today.timeOnly}`);
+      currTempCard.text(`🌡️ ${today.temp}℉`);
+      currHumidCard.text(`${today.humidity}% Humidity`);
+      setSkyConditons();
       setDynamicBackground();
       setDynamicMessage();
       createChart();
@@ -229,37 +208,64 @@ document.addEventListener("DOMContentLoaded", function () {
 
   fetchDataAndCreateChart();
 
+  // Time helper function
+  function separateDTG(defaultDTGstring) {
+    let dtgArray = defaultDTGstring.split("T");
+    today.dateOnly = dtgArray[0];
+    today.timeOnly = dtgArray[1];
+    let timeNumArray = today.timeOnly.split(":");
+    today.timeNum = parseInt(timeNumArray.join(""));
+  }
+
+  // Sky condtions logic ** and skies are ${skyConditions}
+  function setSkyConditons() {
+    if (today.isClear) {
+      today.skyConditions = "clear 😎";
+    } else if (today.isPartlyCloudy) {
+      today.skyConditions = "partly cloudy 🌤️";
+    } else if (today.isCloudy) {
+      today.skyConditions = "cloudy 🌥️";
+    } else {
+      today.skyConditions = "overcast ☁☁️ ";
+    }
+  }
+
   // Logic for dynamic background
   function setDynamicBackground() {
-    if (isDay) {
+    if (today.isDay) {
       // Logic for daytime currContainer
-      if (isRaining) {
-        currContainer.css("background-image", "url(Graphics/day-clear.jpeg)");
+      if (today.isRaining) {
+        // currContainer.css("background-image", "url(Graphics/day-clear.jpeg)");
+      } else if (today.isSnowing) {
+        // currContainer.css("background-image", "url(Add Day snowing)");
       } else {
         currContainer.css("background-image", "url(Graphics/day-clear.jpeg)");
       }
     } else {
-      // Logic for nightime currContainer
-      // nightime Graphics/ezgif.com-video-to-gif.png
-      currContainer.css("background-image", "url(Graphics/night-rain.gif)");
-      // currContainer.css('background-size', 'cover');
+      if (today.isRaining) {
+        currContainer.css("background-image", "url(Graphics/night-rain.gif)");
+      } else if (today.isSnowing) {
+        // currContainer.css  Change to night snow
+      } else {
+        // currContainer.css  Change to night clear
+      }
     }
   }
 
   // Logic for dynamic message
   function setDynamicMessage() {
     let dynamicGreeting;
-    if (timeNum <= 359) {
+    if (today.timeNum <= 359) {
       dynamicGreeting = earlyGreeting;
-    } else if (timeNum > 359 && timeNum <= 1159) {
+    } else if (today.timeNum > 359 && today.timeNum <= 1159) {
       dynamicGreeting = morningGreeting;
-    } else if (timeNum > 1159 && timeNum <= 1700) {
+    } else if (today.timeNum > 1159 && today.timeNum <= 1700) {
       dynamicGreeting = afternoonGreeting;
     } else {
       dynamicGreeting = nightGreeting;
     }
     currMessageCard.text(
-      `Hey ${userName}! ${dynamicGreeting} Today's chance of Rain is ${chanceOfRain}%`
+      `Hey ${today.userName}! ${dynamicGreeting} Today's chance of Rain is ${today.chanceOfRain}% and skies are ${today.skyConditions}`
     );
   }
 
@@ -305,14 +311,15 @@ document.addEventListener("DOMContentLoaded", function () {
       latLongInput.val() !== latLongInput.prop("placeholder")
     ) {
       let latLongArray = latLongInput.val().split(",");
-      latitude = parseFloat(latLongArray[0]);
-      longitude = parseFloat(latLongArray[1]);
+      locationData.latitude = parseFloat(latLongArray[0]);
+      locationData.longitude = parseFloat(latLongArray[1]);
+      getLocWithLatLong();
     } else if (
       zipInput.val() !== "" &&
       zipInput.val() !== zipInput.prop("placeholder")
     ) {
-      defaultZip = parseFloat(zipInput.val());
-      await getLatLong(); // AJAX call to get lat/long
+      locationData.defaultZip = parseFloat(zipInput.val());
+      getLatLong(); // AJAX call to get lat/long
     }
     if (
       usrNameInput.val() !== "" &&
@@ -320,41 +327,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ) {
       userName = usrNameInput.val();
     }
-    console.log(userName);
-    console.log(latitude, longitude);
-    console.log(defaultZip);
+    fetchDataAndCreateChart();
   });
-  
-
-  /*
-  $("#userZip").on("keydown", async function (e) {
-    if (e.keyCode === 13) {
-      defaultZip = parseFloat(zipInput.val());
-      await getLatLong(); // #1 Asyncronous
-      fetchDataAndCreateChart();
-    }
-  });
-*/
-  /*
-  $("#latLongInput").on("keydown", async function (e) {
-    if (e.keyCode === 13) {
-      alert(
-        "Enter Latittude and Longitude separated by comma. \n Example: 47.1, -122.3"
-      );
-      let latLongArray = latLongInput.val().split(",");
-      latitude = parseFloat(latLongArray[0]);
-      longitude = parseFloat(latLongArray[1]);
-      console.log(latitude, longitude);
-      await getLocWithLatLong();
-      await fetchDataAndCreateChart();
-    }
-  });
-  */
 
   // Dropdown button event listener
   setTimeZoneBtn.on("click", function () {
     dropDownContent.toggleClass("show");
-    console.log("Time Zone Button has been clicked");
   });
 
   // Event listener for each timezone option
@@ -362,7 +340,6 @@ document.addEventListener("DOMContentLoaded", function () {
     dropDownContent.removeClass("show"); // Close the dropdown
     locationData.timeZone = encodeURI($(this).text());
     setTimeZoneBtn.text(`${locationData.timeZone} ⬇️`);
-    console.log(locationData.timeZone);
-    console.log("Time Zone Option clicked:", $(this).text());
+    fetchDataAndCreateChart();
   });
 });
